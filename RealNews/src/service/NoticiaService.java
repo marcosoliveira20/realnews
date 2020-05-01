7db10e928c0b7e90e5ab34b62683adaa3ea4b34f
vitor56dantas <61641392+vitor56dantas@users.noreply.github.com>
2020-04-30 22:36:21 -0300

    Add files via upload

---
+++ RealNews/src/service/NoticiaService.java
@@ -0,0 +1,39 @@
+package service;
+
+import model.Noticia;
+
+import java.util.ArrayList;
+
+import dao.NoticiaDAO;
+
+
+public class NoticiaService {
+	NoticiaDAO dao = new NoticiaDAO();
+	
+	public int criar(Noticia noticia) {
+		return dao.criar(noticia);
+	}
+	
+	public void atualizar(Noticia noticia){
+		dao.atualizar(noticia);
+	}
+	
+	public void excluir(int id){
+		dao.excluir(id);
+	}
+	
+	public Noticia carregar(int id){
+		return dao.carregar(id);
+	}
+	public ArrayList<Noticia> buscarNoticias() {
+		try {
+			return dao.buscarNoticias();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	
+	
+}
